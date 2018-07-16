c46720bf841ca87fa0cac5cf36ed0652e9dd056b
Evgeny Yulyugin <evgeny.yulyugin@intel.com>
2018-07-16 10:53:10 +0200

    Move Windows specific files to windows specific folder

--- build-vmlatency.bat
+++ build-vmlatency.bat
@@ -3,7 +3,7 @@
 IF NOT EXIST C:\WinDDK\7600.16385.1 GOTO UNKNOWN_WDK
 
 CALL C:\WinDDK\7600.16385.1\bin\setenv.bat C:\WinDDK\7600.16385.1\ fre x64 WIN7 2>&1 > NUL
-CD /d %~dp0/vmm
+CD /d %~dp0/win
 build /F /w /c
 CD /d %~dp0
 
