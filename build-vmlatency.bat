dd8b2d27cae4370991dc38e7fc7dadf8852b0723
Evgeny Yulyugin <evgeny.yulyugin@intel.com>
2018-07-16 16:33:29 +0200

    Build VMM on Windows

--- build-vmlatency.bat
+++ build-vmlatency.bat
@@ -3,6 +3,12 @@
 IF NOT EXIST C:\WinDDK\7600.16385.1 GOTO UNKNOWN_WDK
 
 CALL C:\WinDDK\7600.16385.1\bin\setenv.bat C:\WinDDK\7600.16385.1\ fre x64 WIN7 2>&1 > NUL
+
+ECHO "Build VMM library"
+CD /d %~dp0/vmm
+build /F /w /c
+
+ECHO "Build vmlatency driver"
 CD /d %~dp0/win
 build /F /w /c
 CD /d %~dp0
