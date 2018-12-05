61a3d44114ea6c2c2dbfcaa7ba726ae5a12b2566
Evgeny Yulyugin <evgeny.yulyugin@intel.com>
2018-12-05 11:00:56 +0100

    Combinte sctipts used for linux and mac os

--- get_vmlatency.sh
+++ get_vmlatency.sh
@@ -1,23 +1,63 @@
 #!/bin/sh
 
-make clean
-make
-
-VMLATENCY_DRIVER=vmlatency.ko
-
-[ -e $VMLATENCY_DRIVER ] || { echo "$VMLATENCY_DRIVER does not exist" ; exit 1 ; }
-
 SUDO=""
 if [ ! -z `id -u` ]; then
-   SUDO="sudo"
+    SUDO="sudo"
 fi
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    VMLATENCY=vmlatency.ko
+    CPU_NAME=`cat /proc/cpuinfo |grep "model name" |uniq |sed s/"^.*: "//`
+
+    function getlog {
+        dmesg
+    }
+
+    function build {
+        make clean
+        make
+    }
+
+    function load {
+        $SUDO /sbin/insmod $VMLATENCY
+    }
+
+    function unload {
+        $SUDO /sbin/rmmod $VMLATENCY
+    }
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    VMLATENCY=build/Release/vmlatency.kext
+    CPU_NAME=`sysctl -n machdep.cpu.brand_string`
+
+    function getlog {
+        $SUDO dmesg
+    }
+
+    function build {
+        xcodebuild
+    }
+
+    function load {
+        $SUDO cp -r $1 /tmp/
+        $SUDO kextload /tmp/vmlatency.kext
+    }
+
+    function unload {
+        $SUDO kextunload /tmp/vmlatency.kext
+    }
+else
+    echo "Unsupported OSTYPE $OSTYPE"
+    exit 1
+fi 
+
+build
+
+[ -e $VMLATENCY ] || { echo "$VMLATENCY does not exist" ; exit 1 ; }
 
 for i in $(seq 1 20)
 do
-    $SUDO /sbin/insmod $VMLATENCY_DRIVER
-    $SUDO /sbin/rmmod $VMLATENCY_DRIVER
+    load $VMLATENCY
+    unload $VMLATENCY
 done
 
-cpu_name=`cat /proc/cpuinfo |grep "model name" |uniq |sed s/"^.*: "//`
-
-dmesg | grep "\[vmlatency\]" | sed s/^"\[ *"[0-9]*\.[0-9]*"\] "// |sed s/"\[vmlatency\] "// > "$cpu_name".txt
+getlog | grep "\[vmlatency\]" | sed s/^"\[ *"[0-9]*\.[0-9]*"\] "// |sed s/"\[vmlatency\] "// > "$CPU_NAME".txt
